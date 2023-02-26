5e7f5694f3592cdb08705d059ca17a7c6e8f82db
MeltemElgun <113603458+MeltemElgun@users.noreply.github.com>
2023-02-26 15:54:37 +0300

    SPA-Routing-tamamlandı.

--- client/src/Filmler/KaydedilenlerListesi.js
+++ client/src/Filmler/KaydedilenlerListesi.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React from "react";
+import { useHistory } from "react-router-dom";
 
 export default function KaydedilenlerListesi(props) {
+  const history = useHistory();
+  const { list } = props;
+
   return (
     <div className="saved-list">
       <h3>Kaydedilen Filmler:</h3>
-      {props.list.map(movie => (
-        <span className="saved-movie">{movie.title}</span>
-      ))}
-      <div className="home-button">Anasayfa</div>
+      {console.log("Kaydedilen", list)}
+      {list
+        ? list.map((movie) => (
+            <span key={movie.id} className="saved-movie">
+              {movie.title}
+            </span>
+          ))
+        : "kaydadilen lisye boş"}
+      <div
+        className="home-button"
+        onClick={() => {
+          history.push("/");
+        }}
+      >
+        Anasayfa
+      </div>
     </div>
   );
 }
